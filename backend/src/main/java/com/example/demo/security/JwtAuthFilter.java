cbebf75b9e062c8ed1ddfdd38e37681230c89751
Hsbalazs <hoffartbalazs@t-online.hu>
2026-06-30 18:29:21 +0200

    adjust JWT rules jwtAuthFilter

--- backend/src/main/java/com/example/demo/security/JwtAuthFilter.java
+++ backend/src/main/java/com/example/demo/security/JwtAuthFilter.java
@@ -30,6 +30,14 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                                     FilterChain filterChain)
             throws ServletException, IOException {
 
+        String path = request.getServletPath();
+
+        // 🔥 LOGIN oldal és LOGIN POST → JWT nélkül is menjen
+        if (path.equals("/auth/login")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         String authHeader = request.getHeader("Authorization");
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
